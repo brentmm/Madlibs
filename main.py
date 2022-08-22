a3463f1e143067ee483b7b7c072e3852add4b966
Brent Martin <77995343+brentmm@users.noreply.github.com>
2022-08-22 02:49:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+#start
+
+"""
+Mad Libs python game 
+"""
+#introduction
+print("Welcome to my Mad Libs game, lets begin!!")
+print("")
+
+#ask for an adjective
+adjective_1 = input("Please enter an adjective: ")
+
+#ask for a plural noun
+noun_plural = input("Please enter a plural noun: ")
+
+#ask for a noun
+noun_1 = input("Please enter a noun: ")
+
+#ask for second adjective
+adjective_2 = input("Enter another adjective: ")
+
+#printing mad lib
+print("")
+print("Roses are " + adjective_1)
+
+print(noun_plural + " are blue")
+
+print(noun_1 + " is " + adjective_2)
+
+print("And so are you!")
+
+"""
+mad libs prt 2
+"""
+#intro to prt 2
+print("")
+print("Now for part Two, lets begin!!")
+print("")
+
+#ask for an adjective
+adjective_a = input("Enter an adjective: ")
+
+#ask for a noun
+noun_a = input("Enter a noun: ")
+
+#ask for a number
+num_a = input("Enter a number: ")
+
+#ask for a noun
+noun_b = input("Enter a noun: ")
+
+#ask for an adjective
+adjective_b = input("Enter a second adjective: ")
+
+#ask for a verb
+verb_a = input("Enter a verb: ")
+
+#ask for a noun
+noun_c = input("Enter a noun: ")
+
+#ask for a body part
+bodypart_a = input("Enter a body part: ")
+
+#ask for an adjective
+adjective_c = input("Enter another adjective: ")
+
+#print mad lib
+print("")
+print("Run away bride proposal")
+print("")
+print("Look, I guarantee there will be " + adjective_a + " times.") 
+print("I guarantee that at some " + noun_a + ", " + num_a + " of us are gonna want to get out of this " + noun_b + ".")
+print("But I also guarantee that if I don`t ask you to be " + adjective_b + ", I`ll " + verb_a + " it for the rest of my " + noun_c + ",")
+print("because I know, in my " + bodypart_a + ", you`re the " + adjective_c + " one for me.") 
+
+#end
